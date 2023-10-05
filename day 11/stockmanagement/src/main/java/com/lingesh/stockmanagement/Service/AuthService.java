7ef46d3df5d8f45b55d270ddd39b8ac9c6a24789
mangalesh123 <135792033+mangalesh123@users.noreply.github.com>
2023-10-05 15:39:03 +0530

    Add files via upload

---
+++ day 11/stockmanagement/src/main/java/com/lingesh/stockmanagement/Service/AuthService.java
@@ -0,0 +1,57 @@
+package com.lingesh.stockmanagement.Service;
+
+import java.util.Optional;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.lingesh.stockmanagement.dto.Response.AuthenticationResponse;
+import com.lingesh.stockmanagement.dto.request.AuthenticationRequest;
+import com.lingesh.stockmanagement.dto.request.RegisterRequest;
+import com.lingesh.stockmanagement.Model.User;
+import com.lingesh.stockmanagement.Model.enumerate.Role;
+import com.lingesh.stockmanagement.Repository.UserRepo;
+import com.lingesh.stockmanagement.util.JwtUtil;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthService  {
+
+    private final UserRepo userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public boolean userRegistration(RegisterRequest request) {
+        Optional<User> isUserExists = userRepository.findByUsername(request.getUsername());
+        if (!isUserExists.isPresent()) {
+            var user = User.builder()
+                    .username(request.getUsername())
+                    .email(request.getEmail())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .isEnabled(true)
+                    .role(Role.valueOf(request.getRole().toUpperCase()))
+                    .build();
+            userRepository.save(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public AuthenticationResponse userAuthentication(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        var user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        var token = jwtUtil.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+}
