384db6c76af67918ed4a6e8f5cf9a86a18882ff2
mangalesh123 <135792033+mangalesh123@users.noreply.github.com>
2023-10-12 10:58:25 +0530

    Add files via upload

---
+++ day 16/src/main/java/com/prabha/taxcalculator/dto/Response/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.prabha.taxcalculator.dto.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+    private String username;
+}
