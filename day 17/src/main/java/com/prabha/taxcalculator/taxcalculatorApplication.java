8159d55451256356928d7c6f687209f24c5989c3
mangalesh123 <135792033+mangalesh123@users.noreply.github.com>
2023-10-12 11:18:36 +0530

    Add files via upload

---
+++ day 17/src/main/java/com/prabha/taxcalculator/taxcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.prabha.taxcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class taxcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(taxcalculatorApplication.class, args);
+	}
+
+}
